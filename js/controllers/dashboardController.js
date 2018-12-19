35808ef0d01e09300cdd9ce5fbc480ee3968106e
harisha7777 <harisha7777@gmail.com>
2018-12-19 12:29:20 +0530

    side bar changes

--- js/controllers/dashboardController.js
+++ js/controllers/dashboardController.js
@@ -1,3 +1,25 @@
-lupaApp.controller("dashboardController",function() {
+lupaApp.controller("dashboardController",function($scope, $timeout) {
+    $scope.expandNav = false;
+         $scope.collapseNav = true;
+         
+         $scope.navToggle = function() {
+             if($scope.expandNav) {
+                 $scope.expandNav = false;
+                 $scope.collapseNav = true;
+             }
+             else {
+                 $scope.expandNav = true;
+                 $scope.collapseNav = false;
+             }
+             
+             
+             
+         }
 
+         $timeout(function() {
+             
+             $("#product li").on("click", function(event) {
+                 event.stopPropagation();
+             })
+         }, 2000);
 });
