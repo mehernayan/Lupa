7409433e15a870299bcf18325f5b8b3ddd1575d3
Narayana <mehernayan@gmail.com>
2019-02-02 17:26:53 +0530

    validate page

--- js/controllers/userValidateController.js
+++ js/controllers/userValidateController.js
@@ -1,5 +1,7 @@
 lupaApp.controller('userValidateController', ['$scope','lupaUserService','$location',
   function($scope,lupaUserService,$location) {
- 
+  $scope.userValidate = function(){
+    $location.path('/user');
+  }
 
 }]);
